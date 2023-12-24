62415584fd1a7a74c83a3e18e02a64bdc1e7bc05
acorriaz <mikey.swx@gmail.com>
2023-12-24 17:16:42 +0700

    Add function to update isHeld of selected choice

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,85 @@
 import './App.css'
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import {nanoid} from "nanoid";
+import {decode} from "html-entities";
 import LandingPage from "./components/LandingPage.jsx";
 import Quiz from "./components/Quiz.jsx";
 
 function App() {
   const [currentPage, setCurrentPage] = useState("landing")
+  const [quizData, setQuizData] = useState([])
+
+  console.log(quizData)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch('https://opentdb.com/api.php?amount=5&category=15&difficulty=medium&type=multiple')
+      const data = await res.json()
+
+      const newData = data.results.map((quiz) => {
+        const { question } = quiz
+        const answers = [...quiz.incorrect_answers, quiz.correct_answer,]
+
+        // Add isHeld to each answers
+        const answersWithHeld = answers.map((answer) => {
+          return {
+            text: decode(answer),
+            isHeld: false
+          }
+        })
+
+        // Shuffle answers
+        answersWithHeld.sort(() => Math.random() - 0.5)
+
+        return {
+          ...quiz,
+          allAnswers: answersWithHeld,
+          question: decode(question)
+        }
+      })
+
+      setQuizData(newData)
+    }
+
+    fetchData()
+  }, [])
 
   function changePage(page) {
     setCurrentPage(page)
   }
+
+  function handleHeld(questionOfAns, choice)  {
+    console.log("choice", choice)
+    console.log("question", questionOfAns)
+    setQuizData((prevQuiz) => {
+      return prevQuiz.map((quizQuestion) => {
+        // find correct choice and flip value
+        if (questionOfAns === quizQuestion.question) {
+          const updatedIsHeldAns = quizQuestion.allAnswers.map((answer) => (
+            answer.text === choice ? {
+                ...answer,
+                isHeld: !answer.isHeld
+            } :
+              answer
+          ))
+          return {
+            ...quizQuestion,
+            allAnswers: updatedIsHeldAns
+          }
+        } else {
+          return quizQuestion
+        }
+      })
+    })
+  }
+
+  const quizComponents = quizData.map((quiz) => (
+    <Quiz
+      key={nanoid()}
+      question={quiz.question}
+      answers={quiz.allAnswers}
+      handleHeld={handleHeld}
+    />))
 
   return (
     <>
@@ -21,7 +92,7 @@ function App() {
       {
         currentPage === "quiz" &&
         <>
-          <Quiz />
+          {quizComponents}
           <button onClick={() => changePage("landing")}>Back</button>
         </>
       }
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,5 +1,21 @@
-export default function Quiz() {
+import {nanoid} from "nanoid";
+
+export default function Quiz(props) {
+  const answersElement = props.answers.map((answer) => (
+    <button
+      key={nanoid()}
+      // TODO: click to update quizData state and add to userSelected state
+      className={`btn ${answer.isHeld ? "btn-selected" : ""}`}
+      onClick={() => props.handleHeld(props.question, answer.text)}
+    >
+      {answer.text}
+    </button>
+  ))
+
   return (
-    <h1>Quiz</h1>
+    <div>
+      <h2>{props.question}</h2>
+      {answersElement}
+    </div>
   )
 }
